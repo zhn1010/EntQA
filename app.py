d5832402aec41c31c14abefc9aef63010cf69372
Saeed Torabzadeh <saeed.torabzadeh@syenah.ai>
2023-04-11 12:43:16 +0200

    use FlatIP index

--- app.py
+++ app.py
@@ -780,7 +780,11 @@ runtime = end_time - start_time
 print(f"reader_model.eval in {runtime}s")
 
 start_time = time.time()
-index = faiss.read_index(args.pretrained_path + "index.ivf")
+# index = faiss.read_index(args.pretrained_path + "index.ivf")
+index = faiss.IndexFlatIP(all_cands_embeds.shape[1])
+if args.use_gpu_index:
+    index = faiss.index_cpu_to_all_gpus(index)
+index.add(all_cands_embeds)
 end_time = time.time()
 runtime = end_time - start_time
 print(f"loading faiss index in {runtime}s")
